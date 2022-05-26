469d94fb03487d969f36a98fa3c264b35473b89d
abrah moh <srjsml12@gmail.com>
2022-05-25 20:31:48 -0600

    add import and export

---
+++ export.js
@@ -0,0 +1,16 @@
+const myFunction = () => {
+
+}
+
+export const HomePage = () => {
+
+}
+
+export const AboutPage = () => {
+
+}
+// const as a module / component 
+
+//  you can only have one per file
+//  bottom line
+export default myFunction;
---
+++ import.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactRouter from 'react-router';
+import { Link, NavLink } from 'react-router';
+// import myFunction from './export';
+// import { HomePage, AboutPage } from './export';
+
+import myFunction, { HomePage, AboutPage } from './export';
+// myFunction()
